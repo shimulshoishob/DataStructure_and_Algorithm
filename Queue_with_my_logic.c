c8adcdc708efe6a3d2f47afcd906d4997cc71adf
Shimul-Kumar <shimulkumarshoishob@gmail.com>
2026-09-23 21:30:23 +0600

    fix some small bugs

--- Queue_with_my_logic.c
+++ Queue_with_my_logic.c
@@ -3,65 +3,48 @@
 int top = -1;
 int queue[CAPACITY];
 
-void enqueue(int item)
-{
-    if (top < CAPACITY - 1)
-    {
-        top++;
-        queue[top] = item;
-        printf("Successfully add item : %d \n", item);
-    }
-    else
-    {
-        printf("Exception, The Queue is full.\n");
-    }
+void enqueue(int item) {
+  if (top < CAPACITY - 1) {
+    top++;
+    queue[top] = item;
+    printf("Successfully add item : %d \n", item);
+  } else {
+    printf("Exception, The Queue is full.\n");
+  }
 }
 
 // normal dequeue
-int normalDequeue()
-{
-    if (top >= 0)
-    {
-        int val = queue[0];
-        for (int i = 0; i < sizeof(queue); i++)
-        {
-            queue[i] = queue[i + 1];
-        }
-        top--;
-        return val;
-    }
-    else
-    {
-        printf("Exception,The normal Queue is empty. \n");
-        return -1;
-    }
-}
-
-
-int showItem()
-{
+int normalDequeue() {
+  if (top >= 0) {
     int val = queue[0];
+    for (int i = 0; i < (int)sizeof(queue); i++) {
+      queue[i] = queue[i + 1];
+    }
+    top--;
     return val;
+  } else {
+    printf("Exception,The normal Queue is empty. \n");
+    return -1;
+  }
 }
 
-int main()
-{
+int showItem() {
+  int val = queue[0];
+  return val;
+}
 
-    printf("Hello, my Queue is ready to work.\n");
-    dequeue();
-    enqueue(10);
-    enqueue(20);
-    enqueue(30);
-    enqueue(40);
-    printf("return item from showItem: %d \n", showItem());
-    printf("return item from dequeue: %d \n", dequeue());
-    enqueue(50);
-    printf("return item from showItem: %d \n", showItem());
+int main() {
 
-    
+  printf("Hello, my Queue is ready to work.\n");
+  normalDequeue();
+  enqueue(10);
+  enqueue(20);
+  enqueue(30);
+  enqueue(40);
+  printf("return item from showItem: %d \n", showItem());
+  printf("return item from dequeue: %d \n", normalDequeue());
+  enqueue(50);
+  printf("return item from showItem: %d \n", showItem());
 
-
-
-
-    return 0;
+  return 0;
 }
